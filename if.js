ef0764fa3422f3aac4bdbce7a7aa78732b83f9cd
GabiDev <115039314+Gabigurgelc@users.noreply.github.com>
2024-02-03 23:37:08 -0300

    Add files via upload

---
+++ if.js
@@ -0,0 +1,12 @@
+var notas_aluno = [7, 6, 8, 5];
+console.log(notas_aluno.length);
+
+
+var soma_notas = notas_aluno[0] + notas_aluno[1] + notas_aluno[2] + 
+notas_aluno[3]
+;
+
+var media_aluno = soma_notas / 4
+;
+
+console.log("Média do aluno: " , media_aluno);
